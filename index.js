d7d60f828c79482fd55eed4f9a6d69cf1fc94283
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-05-19 11:49:31 -0300

    create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const route = require('./routes');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/todo', route.todo);
+
+app.listen(3001, () => {
+  console.log(`Escutando na porta ${3001}`);
+});
